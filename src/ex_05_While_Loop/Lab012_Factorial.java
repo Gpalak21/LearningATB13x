1d8bbd48d05138476aa7befaddcfb3541b8cb44d
Palak <guptapalakagra@gmail.com>
2025-06-27 11:07:44 +0530

    27 June

--- src/ex_05_While_Loop/Lab012_Factorial.java
+++ src/ex_05_While_Loop/Lab012_Factorial.java
@@ -6,10 +6,20 @@ public class Lab012_Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the factorial program \n Enter the number:");
+        if(!scanner.hasNextInt()){
+            System.out.println("Enter the integer your fool.");
+            return;
+        }
         int number = scanner.nextInt();
 
+
         int fact = 1;
-        if(number<0)
+
+        if(number <0)
+        {
+            System.out.println("Negative factorial is not allowed.");
+        }
+        else if(number==0)
         {
             System.out.println(fact);
         }
